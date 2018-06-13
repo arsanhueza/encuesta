45f3c5c0b43c226eb14ebab3963a795a208ba9eb
Arturo Sanhueza <arturosanhueza@macbook-pro-de-arturo.local>
2018-06-13 10:59:18 -0400

    Initial Commit

---
+++ encuesta/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  encuesta
+//
+//  Created by Arturo Sanhueza on 13-06-18.
+//  Copyright © 2018 Arturo Sanhueza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
