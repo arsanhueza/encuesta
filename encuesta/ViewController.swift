17214d4f40715c40f270a539e9a759fdb62f1ca1
Arturo Sanhueza <arturosanhueza@macbook-pro-de-arturo.local>
2018-06-13 12:13:49 -0400

    primer commit proyecto encuesta

--- encuesta/ViewController.swift
+++ encuesta/ViewController.swift
@@ -7,19 +7,53 @@
 //
 
 import UIKit
+import SurveyNative
 
-class ViewController: UIViewController {
-
+class ViewController: SurveyViewController, SurveyAnswerDelegate, CustomConditionDelegate, ValidationFailedDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.setSurveyAnswerDelegate(self)
+        self.setCustomConditionDelegate(self)
+        self.setValidationFailedDelegate(self)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func surveyJsonFile() -> String {
+        return "ExampleQuestions"
     }
-
-
+    
+    override func surveyTitle() -> String {
+        return "Encuesta"
+    }
+    
+    func question(for id: String, answer: Any) {
+        print("Pregunta: \(id) has answer: \(answer)")
+    }
+    
+    func isConditionMet(answers: [String: Any], extra: [String: Any]?) -> Bool {
+        let id = extra!["id"] as! String
+        if id == "check_age" {
+            if let birthYearStr = answers["birthyear"] as? String, let ageStr = answers["age"] as? String {
+                let birthYear = Int(birthYearStr)
+                let age = Int(ageStr)
+                let wiggleRoom = extra!["wiggle_room"] as? Int
+                let date = Date()
+                let calendar = Calendar.current
+                let components = calendar.dateComponents([.year], from: date)
+                let currentYear =  components.year
+                return abs(birthYear! + age! - currentYear!) > wiggleRoom!
+            } else {
+                return false
+            }
+        } else {
+            Logger.log("Unknown custom condition check: \(id)")
+            return false
+        }
+    }
+    
+    func validationFailed(message: String) {
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
 }
 
